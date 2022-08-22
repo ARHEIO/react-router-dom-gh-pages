40a7b42dcd7e81d2d064a99877e0715e8495e5a1
Adam Eggleston <adam.eggleston@estimateone.com>
2022-08-22 12:41:09 +1000

    feat: add features

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Home = () => {
+  return <h2>Home</h2>;
 }
 
-export default App;
+const About = () => {
+  return <h2>About</h2>;
+}
+
+const Users = () => {
+  return <h2>Users</h2>;
+}
+
+
+export const App = () => (
+  <>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>App</h1>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  </>
+);
